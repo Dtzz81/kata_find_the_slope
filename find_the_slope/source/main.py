fc315149d91c3aca06e19746176674cbfc4d1b1d
Dtzz81 <denise.turkova@madetech.com>
2025-01-07 12:12:48 +0000

    last refactoring

--- find_the_slope/source/main.py
+++ find_the_slope/source/main.py
@@ -2,6 +2,6 @@ def find_slope(points):
     x1, y1, x2, y2 = points # uppacking points, can't be the other way around
     if x1 == x2:
         return "undefined"
-    else:
-        slope = (y2 - y1) // (x2 - x1) # integer division
-        return str((slope))
+
+    slope = (y2 - y1) // (x2 - x1) # integer division
+    return str((slope))
