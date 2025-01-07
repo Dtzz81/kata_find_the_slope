45e61471932978add32e8239c3ca1fb4ced4c579
Dtzz81 <denise.turkova@madetech.com>
2025-01-07 09:54:15 +0000

    both tests passing

--- find_the_slope/source/main.py
+++ find_the_slope/source/main.py
@@ -1,2 +1,5 @@
-def find_slope(x1, x2, y1, y2):
-    return "undefined"
+def find_slope(x1, y1, x2, y2):
+    if x1 == x2:
+        return "undefined"
+    else:
+        return (y2 - y1) / (x2 - x1)
